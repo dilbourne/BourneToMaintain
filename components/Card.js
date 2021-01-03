d6f2c4908719697d214eec10af096a0688babd73
Dillon Bourne <dillon_bourne82@live.com>
2021-01-03 17:14:29 -0400

    update cards

--- components/Card.js
+++ components/Card.js
@@ -2,9 +2,9 @@
 
 export const Card = (props) => {
     return (
-        <div className="lg:w-45/100 w-96 my-2 lg:my-6 mx-2 bg-gray-200 overflow-hidden rounded shadow-lg">
+        <div className="w-96 h-full my-2 lg:my-6 mx-2 bg-purple-100 overflow-hidden rounded shadow-lg border-2 border-purple-500 border-opacity-75">
             <img className="w-full" src={props.src} alt="before and after pictures"/>
-            <p className="font-bold text-center text-green-900 uppercase text-sm md:text-lg lg:text-xl px-6 py-1">{props.title}</p>
+            <p className="text-center text-purple-900 uppercase text-sm md:text-lg lg:text-xl px-6 py-1">{props.title}</p>
         </div>
     )
 }
