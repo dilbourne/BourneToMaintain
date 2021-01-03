66c1f8971dc25984daead0970af8bfa69984ebe3
Dillon Bourne <dillon_bourne82@live.com>
2021-01-03 17:14:11 -0400

    add google font Roboto

--- components/Layout.js
+++ components/Layout.js
@@ -13,12 +13,14 @@ const Layout = ({ children, title, description}) => {
                     <title>{title}</title>
                     <link rel="icon" href="/favicon.ico" />
                     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
+                    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400&display=swap" rel="stylesheet"></link>
                 </Head>
                 <div className="container bg-gray-200 mx-auto rounded-sm">
                     <Navbar />
                     <Banner 
-                        title={'Lawn Care - Freighting - Maintenance'}
-                        description={'Call us today for a FREE quote, quick & easy.'}
+                        title={'Lawn Care, Freighting, Maintenance & More!'}
+                        description={'Call us today for a FREE quote!'}
+                        slogan={`"We maintain your yard like it's our very own."`}
                     />
                     {children}
                 </div>
