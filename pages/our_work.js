61cc57a855df23a999968d2b2903b4de8253304e
Dillon Bourne <dillon_bourne82@live.com>
2021-01-05 16:13:28 -0400

    fix carousel to scroll 2 items

--- pages/our_work.js
+++ pages/our_work.js
@@ -31,9 +31,9 @@ export default function OurWork(props) {
             <div className="w-full flex bg-white flex-col justify-center items-center container rounded shadow-lg py-8 px-6 my-8">
                 <h2 className="text-green-700 uppercase mx-auto my-auto text-2xl text-center">OUR WORK</h2>
                 <hr className="text-green-900 text-center mx-auto w-1/5 pt-2"/>
-                <p className="w-3/5 text-base md:text-lg lg:text-xl text-center text-black mt-4">We listen to our customers and give them exactly what they want. Our team works quickly and efficiently - so you don't have to hear noisy equipment all day! We also remove all trimmings and debris from your residence and dispose of it in an eco-friendly manner.</p>
+                <p className="w-4/5 text-base md:text-lg lg:text-xl text-center text-black mt-4">We listen to our customers and give them exactly what they want. Our team works quickly and efficiently - so you don't have to hear noisy equipment all day! We also remove all trimmings and debris from your residence and dispose of it in an eco-friendly manner.</p>
                 <div className="container bg-green-200 h-full rounded shadow-lg mt-8 mb-6 px-4 pt-8 pb-6">
-                    <Carousel breakPoints={breakPoints}>
+                    <Carousel breakPoints={breakPoints} itemsToScroll={2}>
                         {items.map((item)=>(<Card key={item.id} src={folder + item.filename} title={item.title} />))}
                     </Carousel>
                 </div>
